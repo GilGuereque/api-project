8139ea7e99e8e38d6f89a6e4f873fd0e627467a3
GilGuereque <gguereque326@gmail.com>
2023-04-16 15:30:52 -0700

    Created JSON object and Get request

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 // set up express app & required const variables
 const express = require('express');
 const app = express();
-const PORT = 8000
+const PORT = 8000;
+
+// creating JSON object
+const rappers = {
+    '21 Savage': {
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'Chance the Rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'unknown':{
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
+};
 
 // get requests for file system:
 app.get('/', (request,response) => {
     response.sendFile(__dirname + '/index.html');
+});
+
+// get request for JSON 
+app.get('/api', (request,response) => {
+    response.json(rappers);
 });
 
 
