77539362ee409e2256f1cb2dce79410ebe645da6
GilGuereque <gguereque326@gmail.com>
2023-04-16 15:09:24 -0700

    Adding index.html for file system Get requests

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-// set up express app
+// set up express app & required const variables
 const express = require('express');
 const app = express();
+const PORT = 800
 
 // get requests for file system:
 app.get('/', (req,res) => {
     response.sendFile(__dirname + '/index.html');
+});
+
+
+// set up listener to local PORT
+app.listen(PORT, () => {
+    console.log(`Ther server is now running on port ${PORT}! PoggersChampion Kappa`);
 });
